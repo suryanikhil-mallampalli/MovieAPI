e8438b5e21aa9db8841ddf9687f87c7691b3ec81
suryanikhil-mallampalli <suryanikhil_mallampalli@srmap.edu.in>
2023-07-16 17:38:36 +0530

    final commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 import requests
 
 app = Flask(__name__)
@@ -6,79 +6,47 @@ VALID_API_KEY = "260cdb2f0b9569143a580580c4deb0b6"
 MOVIE_API_BASE_URL = "https://api.tmdb.org/3/"
 
 def validate_api_key(api_key):
+    # Function to validate the API key provided by the user
     return api_key == VALID_API_KEY
 
 @app.before_request
 def authenticate_request():
+    # This function is executed before every request is processed.
+    # It checks the API key provided in the query parameters and ensures it is valid.
     api_key = request.args.get("api_key")
     if not api_key or not validate_api_key(api_key):
+        # If the API key is missing or invalid, return a JSON response with an error message and 401 status code.
         return jsonify({"error": "Invalid API key"}), 401
 
-
-
-#1st version
-# # for a certain movie
-# @app.route("/movies/<string:movie_name>", methods=["GET"])
-# def get_movie_details(movie_name):
-#     api_key = request.args.get("api_key")
-#     response = requests.get(f"{MOVIE_API_BASE_URL}search/movie", params={"api_key": api_key, "query": movie_name})
-#     data = response.json()
-#     if "results" in data:
-#         if len(data["results"]) > 0:
-#             movie = data["results"][0]
-#             movie_details = {
-#                 "title": movie["title"],
-#                 "release_year": movie["release_date"][:4],
-#                 "plot": movie["overview"],
-#                 "cast": [],  # You can fetch cast details from a separate API endpoint if available
-#                 "rating": movie["vote_average"],
-#             }
-#             return render_template("movie_details.html", movie=movie_details)
-
-#     return jsonify({"error": "Movie not found"}), 404
-
-# # for all movies
-# @app.route("/movies", methods=["GET"])
-# def get_all_movies():
-#     api_key = request.args.get("api_key")
-#     response = requests.get(f"{MOVIE_API_BASE_URL}discover/movie", params={"api_key": api_key})
-#     data = response.json()
-#     if "results" in data:
-#         movies_list = []
-#         for movie in data["results"]:
-#             movies_list.append(movie["title"])
-#         return render_template("all_movies.html", movies=movies_list)
-
-#     return jsonify({"error": "No movies found"}), 404
-
-
-
-
-# 2nd version
 @app.route("/movies/<string:movie_name>", methods=["GET"])
 def get_movie_details(movie_name):
+    # This route is used to fetch details of a specific movie by its name.
+    # It expects the movie name as a URL parameter.
     api_key = request.args.get("api_key")
     response = requests.get(f"{MOVIE_API_BASE_URL}search/movie", params={"api_key": api_key, "query": movie_name})
     data = response.json()
-    if "results" in data:
-        if len(data["results"]) > 0:
-            movie = data["results"][0]
-            return render_template("movie_details.html", movie=movie)
+    if "results" in data and len(data["results"]) > 0:
+        # If the search result contains movie data, return the details of the first movie as a JSON response.
+        movie = data["results"][0]
+        return jsonify(movie)
 
+    # If the movie is not found, return a JSON response with an error message and 404 status code.
     return jsonify({"error": "Movie not found"}), 404
 
 @app.route("/movies", methods=["GET"])
 def get_all_movies():
+    # This route is used to fetch a list of all available movies.
+    # It does not require any additional parameters.
     api_key = request.args.get("api_key")
     response = requests.get(f"{MOVIE_API_BASE_URL}discover/movie", params={"api_key": api_key})
     data = response.json()
     if "results" in data:
-        return render_template("all_movies.html", movies=data["results"])
+        # If the API response contains a list of movies, return it as a JSON response.
+        return jsonify(data["results"])
 
+    # If no movies are found, return a JSON response with an error message and 404 status code.
     return jsonify({"error": "No movies found"}), 404
 
-
-
-
 if __name__ == "__main__":
+    # Start the Flask app in debug mode.
     app.run(debug=True)
